1a8af74b1d3146132278858470bb6ddccfee8464
Tregub Artem <tregub.artem@gmail.com>
2022-12-03 10:35:50 -0800

    [feature] import activity ktx and compiler exclusion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,13 @@ android {
             findByName(dir)?.java?.srcDirs(project.file("src/$dir/kotlin"))
         }
     }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = freeCompilerArgs + listOf(
+            "-opt-in=kotlinx.coroutines.FlowPreview",
+        )
+    }
     buildFeatures.dataBinding = true
-    kotlinOptions.jvmTarget = "1.8"
     compileOptions.encoding = "UTF-8"
     packagingOptions.resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
 }
@@ -57,19 +62,20 @@ android {
 dependencies {
     arrayOf(
         "androidx.core:core-ktx:1.9.0",
-        "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1",
+        "androidx.lifecycle:lifecycle-runtime-ktx:2.5.1",
         "androidx.appcompat:appcompat:1.5.1",
         "com.google.android.material:material:1.7.0",
         "androidx.constraintlayout:constraintlayout:2.1.4",
         "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0",
         "androidx.recyclerview:recyclerview:1.2.1",
         "com.google.dagger:hilt-android:${Versions.HILT}",
+        "androidx.activity:activity-ktx:1.6.1",
         "com.jakewharton.timber:timber:5.0.1",
         "com.squareup.retrofit2:retrofit:${Versions.RETROFIT}",
         "com.squareup.retrofit2:converter-moshi:${Versions.RETROFIT}",
         "com.squareup.okhttp3:okhttp:${Versions.OK_HTTP}",
         "com.squareup.okhttp3:logging-interceptor:${Versions.OK_HTTP}",
-        "com.squareup.moshi:moshi:${Versions.MOSHI}"
+        "com.squareup.moshi:moshi:${Versions.MOSHI}",
     ).forEach { dependency ->
         implementation(dependency)
     }
