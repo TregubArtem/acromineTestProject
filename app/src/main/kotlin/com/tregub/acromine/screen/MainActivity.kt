dd3eeade1320750b129695058ce3c447d76314c7
Tregub Artem <tregub.artem@gmail.com>
2022-12-03 10:56:32 -0800

    [feature] implement displaying acronym definitions

--- app/src/main/kotlin/com/tregub/acromine/screen/MainActivity.kt
+++ app/src/main/kotlin/com/tregub/acromine/screen/MainActivity.kt
@@ -1,17 +1,49 @@
 package com.tregub.acromine.screen
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tregub.acromine.databinding.ActivityMainBinding
+import com.tregub.acromine.screen.data.MainViewState
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
 
 /**
  * The root class that represents the owner of the entire user interface.
  *  Done without fragments to make it easier to implement.
  */
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private val adapter: DefinitionsAdapter by lazy { DefinitionsAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(ActivityMainBinding.inflate(layoutInflater).root)
+
+        lifecycleScope.launchWhenCreated {
+            viewModel.viewState
+                .filter { state -> state is MainViewState.Success }
+                .map { state -> (state as MainViewState.Success).data }
+                .collect(adapter::submitList)
+        }
+        ActivityMainBinding.inflate(layoutInflater)
+            .also { binding ->
+                binding.lifecycleOwner = this
+                binding.viewModel = viewModel
+                binding.adapter = adapter
+                binding.layoutManager =
+                    LinearLayoutManager(
+                        this,
+                        LinearLayoutManager.VERTICAL,
+                        false,
+                    )
+            }
+            .root
+            .let(::setContentView)
     }
 }
